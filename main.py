974258d31091ad4c541025c76d9f60787982e375
Teo Hughes <hughes.t@etoncollege.org.uk>
2020-05-06 20:31:46 +0100

    Added the model

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ stemmer = LancasterStemmer()
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 
-import numpy
+import numpy as np #ADDED AS NP
 import tflearn
 import tensorflow
 import random
@@ -95,4 +95,20 @@ print("Training data created")
 
 
 
+#Create AI model
+model = Sequential()
+model.add(Dense(128, input_shape = (len(train_x[0]),), activation="relu"))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation="relu"))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation="softmax"))
 
+#Compile model
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
+
+#Fitting and saving the model
+hist = model.fit(np.array(train_x), np.array(train_y), epochs = 200, batch_size = 5, verbose = 1)
+model.save("chatbot_model.h5", hist)
+
+print("model created")
