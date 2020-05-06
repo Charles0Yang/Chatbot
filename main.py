2b5ce182da1738f1b6592bbc0584639410350235
Charles0Yang <yang.c@etoncollege.org.uk>
2020-05-06 11:42:03 +0100

    Added import statements and code to open intents json (also added json file with a few basic intents

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("TEO")
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+import json 
+import pickle
+
+with open("intents.json") as file:
+    data = json.load(file)
