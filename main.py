94584744d984019ddb6199e00dbe243a4c1097e5
Teo Hughes <hughes.t@etoncollege.org.uk>
2020-05-06 19:36:07 +0100

    fixed bugs and stored words and classes

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import tensorflow
 import random
 import json 
 import pickle
-import numpy
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
@@ -22,16 +21,21 @@ classes = []
 documents = []
 ignore_words = ['?', '!']
 
+#Creates a dictionary called intents
 with open("intents.json") as file:
     intents = json.load(file)
     
 #Pre-processing on text and intents via tokenisation
 for intent in intents['intents']:
+    #Goes through the patterns
     for pattern in intent['patterns']:
+        #Tokenize patterns into word (split them into separate words)
         word = nltk.word_tokenize(pattern)
+        #Basically like append
         words.extend(word)
         documents.append((word, intent['tag']))
-        
+
+        #Adds tags to classes
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
@@ -40,5 +44,6 @@ words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words
 words = sorted(list(set(words)))
 classes = sorted(list(set(classes)))
 
+#Stores the words and classes
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
