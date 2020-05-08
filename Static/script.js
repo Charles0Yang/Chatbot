9b3ad07c926b28f2eb1a9724735d23472740cfb2
Charles0Yang <yang.c@etoncollege.org.uk>
2020-05-08 21:12:59 +0100

    Attempt to add javascript to display bot response but error occured

--- Static/script.js
+++ Static/script.js
@@ -4,4 +4,9 @@ function getBotResponse() {
     $("#textInput").val("");
     $("#chatbox").append(userHtml);
     document.getElementById("userInput").scrollIntoView({ block: "start", behavior: "smooth" });
+    $.get("/get", { msg: rawText }).done(function(data) {
+        var botHtml = '<p class="botText"><span>' + data + "</span></p>";
+        $("#chatbox").append(botHtml);
+        document.getElementById("userInput").scrollIntoView({ block: "start", behavior: "smooth" });
+    });
 }
