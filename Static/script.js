328690ba69556b230d7c199babc7907b18bc45aa
Charles0Yang <yang.c@etoncollege.org.uk>
2020-05-08 20:43:23 +0100

    Added a new div in the html called chatbox and updated the getBotResponse function to display the user's input

--- Static/script.js
+++ Static/script.js
@@ -1,3 +1,7 @@
 function getBotResponse() {
-    alert("Enter key has been pressed")
+    var rawText = $("#textInput").val();
+    var userHtml = '<p class="userText"><span>' + rawText + "</span></p>";
+    $("#textInput").val("");
+    $("#chatbox").append(userHtml);
+    document.getElementById("userInput").scrollIntoView({ block: "start", behavior: "smooth" });
 }
