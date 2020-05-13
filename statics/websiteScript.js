4a8cf08d2e5ba62dba223bd98c99da33e60543e2
Teo Hughes <hughes.t@etoncollege.org.uk>
2020-05-13 18:27:00 +0100

    Changed javascript website

--- statics/websiteScript.js
+++ statics/websiteScript.js
@@ -1,9 +1,9 @@
 function getBotResponse() {
     //Displays the user's input 
-    var rawText = $("#textInput").val();
-    var userHtml = '<p class="userText"><span>' + rawText + "</span></p>"; 
-    $("#textInput").val("");
-    $("#chatbox").append(userHtml);
+    var rawText = $("#textInput").val(); //What the user inputs
+    var userHtml = '<p class="userText"><span>' + rawText + "</span></p>"; //Converts it to html
+    $("#textInput").val(""); #Gets a string
+    $("#chatbox").append(userHtml); #Puts the html inside the chatbox
     document.getElementById("userInput").scrollIntoView({ block: "start", behavior: "smooth" }); //Makes it so that the box containing the text will increase smoothly with every input
 
     $.get("/get", { msg: rawText }).done(function(data) { //Goes to the /get page which will return the bot's response as a string
