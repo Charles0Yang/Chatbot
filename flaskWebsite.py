d883dc324e169cd842681be70b67c6b7208f406e
Charles0Yang <yang.c@etoncollege.org.uk>
2020-05-13 17:40:52 +0100

    Added templates folder confirmation

--- flaskWebsite.py
+++ flaskWebsite.py
@@ -29,7 +29,7 @@ words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 def pre_process_input(input):
