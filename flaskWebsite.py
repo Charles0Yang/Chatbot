fa8206c8b3abb8a1c6da152e7f68d49c10a73a0a
Charles0Yang <yang.c@etoncollege.org.uk>
2020-05-14 18:24:13 +0100

    Added feature where bonnie can display answers to a question in a series of messages as opposed to a single blob of text. This is only the case if the keyword long is in the tag of the intent. When displaying the message, Bonnie: doesn't appear yet

--- flaskWebsite.py
+++ flaskWebsite.py
@@ -63,21 +63,32 @@ def get_response(ints, intents_json):
     tag = ints[0]["intent"] #Gets the name of the tag of the intent which the user input belongs to
     list_of_intents = intents_json["intents"] #List of all intents 
     min_accuracy = 0.9993 #Probability above which the response should be valid. If below this number then bot should reply "Sorry I don't understand what you said"
+    result = ""
     for i in list_of_intents:
         if(i["tag"] == tag): #If the intent of the user input matches the intent
             print(float(ints[0]["probability"])) #Print statement to check the certainty of the model of the user's input. Use to error check
-            if float(ints[0]["probability"]) > min_accuracy: #The model's probability of the response being valid must be above the min_accuracy otherwise the response may be invalid
+            if float(ints[0]["probability"]) > min_accuracy and "long" not in tag: #The model's probability of the response being valid must be above the min_accuracy otherwise the response may be invalid
                 result = random.choice(i["responses"]) #Picks a random response from the list of responses in the intent of the user message
+                break
+            elif float(ints[0]["probability"]) > min_accuracy and "long" in tag:
+                result = i["responses"]
                 break
             else:
                 result = "Sorry I don't understand what you said" #Returns generic "I don't understand message instead of a listed response
+                break
     return result
 
 def chatbot_response(input):
+    response_list = []
     intent_of_input = predict_class(input, model) #Gets the intent of the user input
     response = get_response(intent_of_input, intents) #Gets a random response based off the intent of the user input
-    
-    return("Bonnie: {}".format(response)
+    if "long" not in intent_of_input[0]["intent"]:
+        formatted_response = str(("Bonnie: {}".format(response)))
+        response_list.append(formatted_response)
+    else:
+        for sentence in response:
+            response_list.append(sentence)
+    return str(response_list)
 
 
 @app.route('/') #Home page
@@ -91,9 +102,9 @@ def get_bot_response():
     with graph.as_default(): #Important for the model to be generated and utilised correctly
         set_session(sess) 
         userText = request.args.get('msg') #Gets the user input from the textbox
-        response = str(chatbot_response(userText)) #Returns the appropriate response to the user's input to be used in the javascript file so that the bot's response can be displayed
-        return response
+        return chatbot_response(userText) #Returns the appropriate response to the user's input to be used in the javascript file so that the bot's response can be displayed
 
+            
 
 if __name__ == "__main__":
     app.run(debug=True) 
