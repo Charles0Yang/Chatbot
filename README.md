a0fd593db01e970c1df27b9f6fa9eaee870fbd49
Charles0Yang <yang.c@etoncollege.org.uk>
2020-05-14 12:27:57 +0100

    Changed javscript comments to correct format

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Chatbot
+# Bonnie Bot
+
+Bonnie Bot is a Chatbot we have created to help ...
+
+## How to access
+
+Simply click [here](https://bonniebot.herokuapp.com/)!
+
+## Current types of sentence Bonnie Bot will respond to
+
+Hello!
+How are you?
+Goodbye!
+What is your name?
+Can we be friends?  
+What do you do?
+What is your favourite football team?
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- statics/websiteScript.js
+++ statics/websiteScript.js
@@ -2,8 +2,8 @@ function getBotResponse() {
     //Displays the user's input 
     var rawText = $("#textInput").val(); //What the user inputs
     var userHtml = '<p class="userText"><span>' + rawText + "</span></p>"; //Converts it to html
-    $("#textInput").val(""); #Gets a string
-    $("#chatbox").append(userHtml); #Puts the html inside the chatbox
+    $("#textInput").val(""); //Gets a string
+    $("#chatbox").append(userHtml); //Puts the html inside the chatbox
     document.getElementById("userInput").scrollIntoView({ block: "start", behavior: "smooth" }); //Makes it so that the box containing the text will increase smoothly with every input
 
     $.get("/get", { msg: rawText }).done(function(data) { //Goes to the /get page which will return the bot's response as a string
