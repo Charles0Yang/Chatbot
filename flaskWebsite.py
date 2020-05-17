1982efbee07ee4777624365014ca76c43ab2fc99
Charles0Yang <yang.c@etoncollege.org.uk>
2020-05-17 17:21:24 +0100

    Removed import of gtts to fix crash bug

--- flaskWebsite.py
+++ flaskWebsite.py
@@ -6,7 +6,6 @@ Created on Wed May  6 19:32:38 2020
 """
 from flask import Flask, render_template, request
 import os
-from gtts import gTTS
 import nltk
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
