8b1dc7a334a713c45a41de368e0c98b8a0cb63bf
Teo Hughes <hughes.t@etoncollege.org.uk>
2020-05-20 13:35:34 +0100

    Added dropdown menu

--- statics/websiteScript.js
+++ statics/websiteScript.js
@@ -25,12 +25,3 @@ function getBotResponse() {
         }
     });
 }
-/*function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-*/
