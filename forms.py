23e00ba1d7a6c12d9c4628bbe87c9fa06aecdf8f
Teo Hughes <hughes.t@etoncollege.org.uk>
2020-06-04 16:55:40 +0100

    Commented forms.py page

--- forms.py
+++ forms.py
@@ -1,10 +1,16 @@
+#Python to create a form
+
+
+#Imports the libraries we need to create a form
 from flask_wtf import Form
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import InputRequired, Email
- 
+
+#Creates a contact form class
 class ContactForm(Form):
+  #Creates the input fields and messages for the form
   name = StringField("Name",  [InputRequired("Please enter your name.")])
   email = StringField("Email",  [InputRequired("Please enter your email address.")])
   subject = StringField("Subject",  [InputRequired("Please enter a subject.")])
-  message = TextAreaField("Message",  [InputRequired("Not including a message would be stupid")])
+  message = TextAreaField("Message",  [InputRequired("Not including a message would be very silly! :)")])
   submit = SubmitField("Send")
